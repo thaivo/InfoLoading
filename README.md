6bdedefcc99c2ee881960b10bbaa2b69164ad5c6
thaivo <thai2903@gmail.com>
2021-10-04 21:41:07 -0400

    - Added README.md file. - Added backup of database. - Removed node_modules under server directory.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Description: 
+a simple web application that loads the images and usernames uses Vue for front-end, Node for the back-end, and MySQL for database.
+
+# Installation:
+Install MySQL and import userdb.sql file under database directory
+Here is database connection detail of Node API server:
+host : 'localhost'
+user: 'root'
+password : ''
+database : 'userdb'
+
+# Steps to run: 
+## To start API server:
+cd server
+npm install
+npm start
+
+## To start vue client:
+cd vue_client
+npm install
+npm run serve
+
---
+++ database/userdb.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 05, 2021 at 03:31 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `userdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` char(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`) VALUES
+(1, 'AlexFerguson'),
+(2, 'EricCantona'),
+(3, 'RoyKeane'),
+(4, 'PaulScholes'),
+(5, 'WayneRooney'),
+(6, 'CristianoRonaldo');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
